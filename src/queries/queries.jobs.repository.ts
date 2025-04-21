0583d18f7ad6e361b7257330d0bb2d0ad864979b
Dev <devanandgowda8@gmail.com>
2025-04-21 12:16:19 +0530

    feat(queries): add `background` field to execute endpoint for long running queries
    Closes #153

--- src/queries/queries.jobs.repository.ts
+++ src/queries/queries.jobs.repository.ts
@@ -1,5 +1,10 @@
 import { Effect as E, pipe } from "effect";
-import { type StrictFilter } from "mongodb";
+import {
+  type InsertOneResult,
+  type StrictFilter,
+  type StrictUpdateFilter,
+  UUID,
+} from "mongodb";
 import {
   DatabaseError,
   DocumentNotFoundError,
@@ -8,6 +13,39 @@ import {
 import { CollectionName, checkPrimaryCollectionExists } from "#/common/mongo";
 import type { AppBindings } from "#/env";
 import type { QueryJobDocument } from "./queries.types";
+
+export function toQueryJobDocument(queryId: UUID): QueryJobDocument {
+  const now = new Date();
+
+  return {
+    _id: new UUID(),
+    _created: now,
+    _updated: now,
+    status: "pending",
+    queryId,
+  };
+}
+
+export function insert(
+  ctx: AppBindings,
+  document: QueryJobDocument,
+): E.Effect<
+  InsertOneResult<QueryJobDocument>,
+  PrimaryCollectionNotFoundError | DatabaseError
+> {
+  return pipe(
+    checkPrimaryCollectionExists<QueryJobDocument>(
+      ctx,
+      CollectionName.JobsQueries,
+    ),
+    E.flatMap((collection) =>
+      E.tryPromise({
+        try: () => collection.insertOne(document),
+        catch: (cause) => new DatabaseError({ cause, message: "insert" }),
+      }),
+    ),
+  );
+}
 
 export function findOne(
   ctx: AppBindings,
@@ -25,6 +63,43 @@ export function findOne(
       E.tryPromise({
         try: () => collection.findOne(filter),
         catch: (cause) => new DatabaseError({ cause, message: "findOne" }),
+      }),
+    ),
+    E.flatMap((document) =>
+      document
+        ? E.succeed(document)
+        : E.fail(
+            new DocumentNotFoundError({
+              collection: CollectionName.JobsQueries,
+              filter,
+            }),
+          ),
+    ),
+  );
+}
+
+export function updateOne(
+  ctx: AppBindings,
+  jobId: UUID,
+  data: Partial<QueryJobDocument>,
+): E.Effect<
+  void,
+  DocumentNotFoundError | PrimaryCollectionNotFoundError | DatabaseError
+> {
+  const filter: StrictFilter<QueryJobDocument> = { _id: jobId };
+  const update: StrictUpdateFilter<QueryJobDocument> = {
+    $set: data,
+  };
+
+  return pipe(
+    checkPrimaryCollectionExists<QueryJobDocument>(
+      ctx,
+      CollectionName.JobsQueries,
+    ),
+    E.flatMap((collection) =>
+      E.tryPromise({
+        try: () => collection.updateOne(filter, update),
+        catch: (cause) => new DatabaseError({ cause, message: "updateOne" }),
       }),
     ),
     E.flatMap((document) =>
