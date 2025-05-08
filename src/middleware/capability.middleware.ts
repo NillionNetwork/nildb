db0fe8a4e5ec7c49190067850aebef9bc7f1f5df
Tim Holmes-Mitra <tim.holmesmitra@nillion.com>
2025-05-08 09:37:03 +0100

    feat(DROP-ME): hard code account for MCP/openai tool calling demo

--- src/middleware/capability.middleware.ts
+++ src/middleware/capability.middleware.ts
@@ -1,19 +1,18 @@
 import {
   type Command,
   Did,
-  NilauthClient,
   type NucToken,
   NucTokenEnvelopeSchema,
   NucTokenValidator,
   ValidationParameters,
 } from "@nillion/nuc";
 import { Effect as E, pipe } from "effect";
-import type { MiddlewareHandler, Next } from "hono";
+import type { Context, Input, MiddlewareHandler, Next } from "hono";
 import { StatusCodes, getReasonPhrase } from "http-status-codes";
 import { z } from "zod";
 import * as AccountsRepository from "#/accounts/accounts.repository";
 import type { Path } from "#/common/paths";
-import type { AppBindings, AppContext } from "#/env";
+import type { AppBindings, AppContext, AppEnv } from "#/env";
 
 export function verifyNucAndLoadSubject(
   bindings: AppBindings,
@@ -21,12 +20,17 @@ export function verifyNucAndLoadSubject(
   const { log, config } = bindings;
 
   const nilauthDid = Did.fromHex(config.nilauthPubKey);
-  const defaultValidationParameters = new ValidationParameters();
-  const validateNucWithSubscription = new NucTokenValidator([nilauthDid]);
 
   return async (c: AppContext, next: Next) => {
     try {
-      const authHeader = c.req.header("Authorization") ?? "";
+      // TODO(tim): 'now' is fixed in these to the moment of instantiation not when the validation is called
+      //  now should be determined dynamically when validation is called
+      const defaultValidationParameters = new ValidationParameters();
+      const validateNucWithSubscription = new NucTokenValidator([nilauthDid]);
+
+      // hardcoded for demo to sidestep figuring out auth in the flow: owui frontend --> owui server -[auth]-> nildb
+      const authHeader =
+        "Bearer eyJhbGciOiJFUzI1NksifQ.eyJpc3MiOiJkaWQ6bmlsOjAyMDAzNzNmYzcwNTk4ZjQ0MmJhNGNmNjdiOGYzMTVlZjRkNjU3ZTJlMjk4NmU4NDIyOTAyNjc1ZTVhNDRjMGRiZCIsImF1ZCI6ImRpZDpuaWw6MDJkMWYxOThkZjlhNjRmZmEyN2MyOTM4NjFiYWNlOGM4MGJkNmIxZTE1MGUwMDgyNjdmN2Y5NGVhZTllNmMzODBjIiwic3ViIjoiZGlkOm5pbDowMjAwMzczZmM3MDU5OGY0NDJiYTRjZjY3YjhmMzE1ZWY0ZDY1N2UyZTI5ODZlODQyMjkwMjY3NWU1YTQ0YzBkYmQiLCJjbWQiOiIvbmlsL2RiIiwiYXJncyI6e30sIm5vbmNlIjoiYjYyODVlNGQ2MmUxNjkyZDFkNzM2NTRiM2E3NDIyNDYiLCJwcmYiOlsiM2Q3MDU5ZTdkNTRlNzE1NjE0ODFkOTVhNGZiNDcwNjgwYjE2ZjkyMTg1YjU2ZjMwNzY3ZThlMDNkZjlkNmJjNCJdfQ.vP1EYRnNT0quGt5fWMwWrpLAZqRWifGMY1PxCIudXmwTHVWzVlIgK398Q5DPLItdwjhSnHOc-iLc7luOHR5b3g/eyJhbGciOiJFUzI1NksifQ.eyJpc3MiOiJkaWQ6bmlsOjAyMDAzNzNmYzcwNTk4ZjQ0MmJhNGNmNjdiOGYzMTVlZjRkNjU3ZTJlMjk4NmU4NDIyOTAyNjc1ZTVhNDRjMGRiZCIsImF1ZCI6ImRpZDpuaWw6MDIwMDM3M2ZjNzA1OThmNDQyYmE0Y2Y2N2I4ZjMxNWVmNGQ2NTdlMmUyOTg2ZTg0MjI5MDI2NzVlNWE0NGMwZGJkIiwic3ViIjoiZGlkOm5pbDowMjAwMzczZmM3MDU5OGY0NDJiYTRjZjY3YjhmMzE1ZWY0ZDY1N2UyZTI5ODZlODQyMjkwMjY3NWU1YTQ0YzBkYmQiLCJjbWQiOiIvbmlsL2RiIiwicG9sIjpbXSwibm9uY2UiOiI0YWVkMTY0ZWQzYzc1ODgzZjJlY2YzNWE1MTQxNDdmMCJ9.ETAlK27Wh2l0xVK-0j85d9rE6BRTR5xMrh4XEFkZ3H91MPwp4XpTROBCayDQeLZwXAq3cGFXvmE-LhfB37-Nug";
       const [scheme, tokenString] = authHeader.split(" ");
       if (scheme.toLowerCase() !== "bearer") {
         return c.text(
@@ -63,31 +67,31 @@ export function verifyNucAndLoadSubject(
       }
 
       // both branches throw on validation failure
-      if (account._type === RoleSchema.enum.organization) {
-        validateNucWithSubscription.validate(
-          envelope,
-          defaultValidationParameters,
-        );
-        // check revocations last because it's costly (in terms of network RTT)
-        const { revoked } = await NilauthClient.findRevocationsInProofChain(
-          config.nilauthBaseUrl,
-          envelope,
-        );
-        if (revoked.length !== 0) {
-          const hashes = revoked.map((r) => r.tokenHash).join(",");
-          log.warn(
-            "Token revoked: revoked_hashes=(%s) auth_token=%O",
-            hashes,
-            envelope.token.token.toJson(),
-          );
-          return c.text(
-            getReasonPhrase(StatusCodes.UNAUTHORIZED),
-            StatusCodes.UNAUTHORIZED,
-          );
-        }
-      } else {
-        envelope.validateSignatures();
-      }
+      // if (account._type === RoleSchema.enum.organization) {
+      //   validateNucWithSubscription.validate(
+      //     envelope,
+      //     defaultValidationParameters,
+      //   );
+      //   // check revocations last because it's costly (in terms of network RTT)
+      //   const { revoked } = await NilauthClient.findRevocationsInProofChain(
+      //     config.nilauthBaseUrl,
+      //     envelope,
+      //   );
+      //   if (revoked.length !== 0) {
+      //     const hashes = revoked.map((r) => r.tokenHash).join(",");
+      //     log.warn(
+      //       "Token revoked: revoked_hashes=(%s) auth_token=%O",
+      //       hashes,
+      //       envelope.token.token.toJson(),
+      //     );
+      //     return c.text(
+      //       getReasonPhrase(StatusCodes.UNAUTHORIZED),
+      //       StatusCodes.UNAUTHORIZED,
+      //     );
+      //   }
+      // } else {
+      //   envelope.validateSignatures();
+      // }
 
       c.set("envelope", envelope);
       c.set("account", account);
@@ -106,11 +110,19 @@ export function verifyNucAndLoadSubject(
 export const RoleSchema = z.enum(["root", "admin", "organization", "user"]);
 export type Role = z.infer<typeof RoleSchema>;
 
-export type EnforceCapabilityOptions = {
+export type ValidatedJsonInput<T> = Input & {
+  out: {
+    json: T;
+  };
+};
+
+// biome-ignore lint/complexity/noBannedTypes: following hono's expected types
+export type EnforceCapabilityOptions<I extends Input = {}> = {
   path: Path;
   cmd: Command;
   roles: Role[];
-  validate: (c: AppContext, token: NucToken) => boolean;
+  // biome-ignore lint/suspicious/noExplicitAny: following hono's expected types
+  validate: (c: Context<AppEnv, any, I>, token: NucToken) => boolean;
 };
 
 export function enforceCapability(
